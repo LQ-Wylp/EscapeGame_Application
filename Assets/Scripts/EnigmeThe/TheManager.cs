1037016ffc1cb5dd946a295a04b145a8031ff021
Lea Vlieghe <LVlieghe2001@hotmail.com>
2021-02-18 15:32:26 +0100

    machine the

---
+++ Assets/Scripts/EnigmeThe/TheManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TheManager : MonoBehaviour
+{
+    public List<int> bonneCombi;
+
+    public UnityEvent endEvent;
+    public UnityEvent goodEvent;
+    public Text textRecette;
+
+    float NumberOne;
+    float NumberTwo;
+    float NumberThree;
+    int Index = 0;
+    int recipe = 1;
+
+    void Start()
+    {
+        NumberOne = 0;
+        NumberTwo = 0;
+        NumberThree = 0;
+
+        Index = 0;
+    }
+
+    public void ChangeNumber(int Number)
+    {
+        if (Index == 0)
+        {
+            NumberOne = Number;
+        }
+        if (Index == 1)
+        {
+            NumberTwo = Number;
+        }
+        if (Index == 2)
+        {
+            NumberThree = Number;
+        }
+
+        Index++;
+
+        if (Index >= 3)
+        {
+            CheckCombi();
+            Index = 0;
+        }
+        
+    }
+
+    public void CheckCombi()
+    {
+        float CodeEntrer = NumberOne * 1000 + NumberTwo * 100 + NumberThree;
+
+        if (bonneCombi[0] == CodeEntrer)
+        {
+            if (recipe == 3)
+            {
+                endEvent.Invoke();
+            }
+            else
+            {
+                recipe++;
+                textRecette.text = "Recette " + recipe;
+
+                goodEvent.Invoke();
+
+                bonneCombi.RemoveAt(0);
+            }  
+
+        }
+        else
+        {
+            NumberOne = 0;
+            NumberTwo = 0;
+            NumberThree = 0;
+
+            Index = 0;
+        }
+    }
+}
