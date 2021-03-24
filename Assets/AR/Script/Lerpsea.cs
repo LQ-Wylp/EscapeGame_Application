12dd520fc1e9965e25532da5808f67ef55b2f0b4
Boussin Pierre <boussinpierre@gmail.com>
2021-03-24 17:03:23 +0100

    add sound

---
+++ Assets/AR/Script/Lerpsea.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerpsea : MonoBehaviour
+{
+    public Vector3 a;
+    public Vector3 b;
+    [Range(0, 1)]
+    public float pourcentage;
+    public float speed;
+
+   
+    public AnimationCurve curve;
+
+    private void Start()
+    {
+        a += transform.position;
+        b += transform.position;
+    }
+
+    void Update()
+
+    {
+       //effectue un lerp entre deux positions (transition linéaire)
+            pourcentage += Time.deltaTime *speed;
+       
+        
+        float easing = curve.Evaluate(pourcentage);
+
+        transform.position = Vector3.Lerp(a, b, easing);
+        if (pourcentage >= 1)
+        {
+            Destroy(this);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(a, Vector3.one * 0.5f);
+        Gizmos.DrawCube(b, Vector3.one * 0.5f);
+    }
+}
+
