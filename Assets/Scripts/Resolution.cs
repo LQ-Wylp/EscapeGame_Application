0d139abdedcf44bb786cc24815b3f9c939224db6
Leuliet Quentin <Quentin.02300@hotmail.fr>
2021-03-31 10:46:42 +0200

    fzezefez

---
+++ Assets/Scripts/Resolution.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    private void Awake() 
+    {
+        Screen.SetResolution(1080,1920,true);
+    }
+}
